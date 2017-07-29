dd81d72d369942801c8088035c8649799b437a2d
mikemurray86 <mikemurray86@gmail.com>
2017-07-29 00:50:24 -0600

    initial verson of a setup script to copy files into place

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Tests if config files are already present and moves them if they exist before linking the files
+[ -f ${HOME}/.bashrc ] &&  mv ${HOME}/.bashrc ${HOME}/.bashrc_orig
+ln ./bashrc ${HOME}/.bashrc
+echo "${HOME}/.bashrc set"
+
+[ -f ${HOME}/.vimrc ] && mv ${HOME}/.vimrc ${HOME}/.vimrc_orig
+ln ./vimrc ${HOME}/.vimrc
+echo "${HOME}/.vimrc set"
+
+if type -p tmux >/dev/null 2>/dev/null
+then
+	[ -f ${HOME}/.tmux.conf ] && mv ${HOME}/.tmux.conf ${HOME}/.tmux.conf.orig
+	ln ./tmux.conf ${HOME}/.tmux.conf
+	echo "${HOME}/.tmux.conf set"
+fi
