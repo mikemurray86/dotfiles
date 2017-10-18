eedcf2e6776942f9932440cf80321af1f7b7269f
mikemurray86 <mikemurray86@gmail.com>
2017-10-17 22:06:26 -0600

    added additional setup logic to install git.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,41 @@
 #! /bin/bash
 
+function install_software (){
+	os=$( grep -P -o '(?<=^NAME=).+' /etc/os-release )
+	case $os in
+		"*Arch*")
+			sudo pacman -S $1
+			;;
+		"*Ubuntu*")
+			sudo apt -y install $1
+			;;
+		"*Fedora*")
+			sudo dnf install $1
+			;;
+		*)
+			echo "This OS is not supported"
+			exit 1
+			;;
+	esac
+}
+function airline_prep (){
+	if ! type -p git &> /dev/null
+	then
+		read -p 'Did not find Git. Should it be installed? (y/n) ' ans
+		if ans -eq 'y'
+		then
+			install_software git
+		else
+			echo "Git not installed. Please edit the .vimrc to remove airline."
+		fi
+	fi
+}
 # Tests if config files are already present and moves them if they exist before linking the files
 [ -f ${HOME}/.bashrc ] &&  mv ${HOME}/.bashrc ${HOME}/.bashrc_orig
 ln ./bashrc ${HOME}/.bashrc
 echo "${HOME}/.bashrc set"
 
+airline_prep
 [ -f ${HOME}/.vimrc ] && mv ${HOME}/.vimrc ${HOME}/.vimrc_orig
 ln ./vimrc ${HOME}/.vimrc
 echo "${HOME}/.vimrc set"
